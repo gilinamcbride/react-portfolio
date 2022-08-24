85bf8448c23a0cdc1a967387bc0c3ff1ef95c61a
Gilina McBride <gilinamcbride@gmail.com>
2022-08-24 15:32:10 -0400

    add new skills icons and finish section

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
-// import icon from '../../assets/images/icons/icons8-html-5-50.png'
+import React from "react";
 
 const Skills = () => {
     const photos = [
-        "icons8-html-5-50", "icons8-css3-50", "icons8-javascript-logo-50", "icons8-nodejs-50", "icons8-npm-50", "icons8-mysql-logo-50", "icons8-github-50"
+        {photo: "icons8-html-5-50",
+        name: "html logo"},
+        {photo: "icons8-css3-50",
+        name: "css logo"},
+        {photo: "icons8-javascript-logo-50",
+        name: "javascript logo"},
+        {photo: "icons8-nodejs-50",
+        name: "node logo"},
+        {photo: "icons8-npm-50",
+        name: "npm logo"},
+        {photo: "icons8-mysql-logo-50",
+        name: "mysql logo"},
+        {photo: "icons8-github-50",
+        name: "github logo"},
+        {photo: "icons8-react-50",
+        name: "react logo"},
+        {photo: "icons8-mongodb-50",
+        name: "mongo logo"}
 ]
     return(
         <section className="skills">
             <h2>Skills</h2>
             <div className="icons">
-                <div>
-                    {photos.map((photo) => (
-                        <img
-                        src={`../../assets/images/icons/${photo}.png`}
-                        alt="pretty icon" />
-                    ))}
-                </div>
-                {/* <div><img src={icon} alt="HTML logo" /></div> */}
-
+                {photos.map((photo) => (
+                    <div><img
+                    src={require(`../../assets/images/icons/${photo.photo}.png`)}
+                    alt={photo.name} /></div>
+                ))}
             </div>
         </section>
     )
