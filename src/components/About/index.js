64e85f359b5a74d39760fe64841d3d83cc8d5118
Gilina McBride <gilinamcbride@gmail.com>
2022-10-04 12:56:33 -0400

    updated bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
-const About = ()=> {
-    return(
-        <section id="about" className="about-me">
-            <section className="personal-info">
-                <h1>Hi! I'm Gilina McBride.</h1>
-                <p>I am a full-stack developer leveraging a background in communications to build strong user experiences on the web. I am currently earning a certificate in Full Stack Development from the University of North Carolina at Chapel Hill and have developed skills in HTML, CSS, JavaScript ES6, Node.js, SQL, mobile-first design, and more. As an experienced communicator with a strong combination of collaboration, problem-solving, and project management skills, I can design, build, and maintain websites from conception to production. Previous experiences have provided me the ability to work in fast-paced and diverse team environments creating solutions and collaborating cross-functionally with several teams towards a single, shared goal. My strengths lie in my ability to organize, lead others, and innovate.
-                </p>
-            </section>
-            <section className="photo"></section>
-        </section>
-    )
+const About = () => {
+  return (
+    <section id='about' className='about-me'>
+      <section className='personal-info'>
+        <h1>Hi! I'm Gilina McBride.</h1>
+        <p>
+          I am a full-stack developer leveraging a background in communications
+          to build strong user experiences on the web. I have recently earned a
+          certificate in Full Stack Web Development from the University of North
+          Carolina at Chapel Hill and have developed skills in HTML, CSS,
+          JavaScript ES6, Node.js, SQL, MongoDB, React, mobile-first design, and
+          more. As an experienced communicator with a strong combination of
+          collaboration, problem-solving, and project management skills, I can
+          design, build, and maintain websites from conception to production.
+          Previous experiences have provided me the ability to work in
+          fast-paced and diverse team environments creating solutions and
+          collaborating cross-functionally with several teams towards a single,
+          shared goal. My strengths lie in my ability to organize, lead others,
+          and innovate.
+        </p>
+      </section>
+      <section className='photo'></section>
+    </section>
+  );
 };
 
 export default About;
